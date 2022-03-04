eec78badcfc4796da5ce16e0c2886fa445a995d2
TStr626 <96715393+TStr626@users.noreply.github.com>
2022-03-04 14:12:07 +0900

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+#include<cmath>
+#include<numeric>
+#include<string>
+#include<algorithm>
+#include<map>
+#include<set>
+#include<queue>
+#include<iomanip>
+using namespace std;
+
+# define rep(i,n) for(i=0; i<n; i++)
+
+int main() {
+	long long n, m, i, j, k, h, w, x, y, ans, cur, res, jud, mod;
+	
+}
